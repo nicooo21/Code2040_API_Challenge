3e154481a4270b92e2bf3fbe05575fda80a24cf0
nicooo21 <nmagana2112@gmail.com>
2016-08-26 23:06:18 -0700

    Added Comments to Step 3

--- code2040_step3.py
+++ code2040_step3.py
@@ -13,9 +13,9 @@ def main():
 	needle = data["needle"]
 	index = 0
 	haystack = data["haystack"]
-	for x in range(0, len(haystack)):
-		if haystack[x] == needle:
-			index = x
+	for x in range(0, len(haystack)): #iterate through 'haystack' array
+		if haystack[x] == needle: #check if element in haystack has needle
+			index = x 	  #assigns that index
 	url = 'http://challenge.code2040.org/api/haystack/validate'
 	m_data = { 'token':'4db4fe513b59797d9ed7b6841cfbbf89' , 'needle':index}		
 	r = requests.post(url, data=json.dumps(m_data), headers=headers)
