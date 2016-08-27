d8bdafe2a026cbb1347087ab295376ba3fde8ead
nicooo21 <nmagana2112@gmail.com>
2016-08-26 23:09:59 -0700

    Added Comments to Step 5
    Also took out prints used debugging purposes

--- code2040_step5.py
+++ code2040_step5.py
@@ -15,14 +15,11 @@ def main():
 	data = json.loads(array)
 	date_string = data["datestamp"]
 	seconds_str= data["interval"]
-	seconds_int = int(seconds_str)
-	original_datetime = parser.parse(date_string)
-	print original_datetime
-	print seconds_int
-	new_time = original_datetime + timedelta(seconds = seconds_int)
-	new_time1 = new_time.replace(tzinfo=None)
-	print new_time1
-	finaltime = new_time1.strftime('%Y-%m-%dT%H:%M:%SZ')
+	seconds_int = int(seconds_str)			# converts seconds to an int
+	original_datetime = parser.parse(date_string)	# converts string to a datetime
+	new_time = original_datetime + timedelta(seconds = seconds_int)	 # used timedelta to add seconds to the original datetime
+	new_time1 = new_time.replace(tzinfo=None)			# took out timezone
+	finaltime = new_time1.strftime('%Y-%m-%dT%H:%M:%SZ')		# converted and formatted back into a string
 	url = 'http://challenge.code2040.org/api/dating/validate'
 	m_data = { 'token':'4db4fe513b59797d9ed7b6841cfbbf89', 'datestamp':finaltime}
 	r = requests.post(url, data=json.dumps(m_data), headers=headers)
