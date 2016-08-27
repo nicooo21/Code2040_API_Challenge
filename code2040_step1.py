a43217526ab0f86193fe99e40584203f96c1d865
nicooo21 <nmagana2112@gmail.com>
2016-08-26 19:56:17 -0700

    Uploaded wrong file

--- code2040_step1.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def main():
-	request = requests.post("http://challenge.code2040.org/api/register", json = {"key":"value"})
-	print request.status_code
-
-if __name__ == "__main__":
-	main()
