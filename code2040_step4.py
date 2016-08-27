e38ac5e3ec5d67fea07e0d16f159384eff50ba9e
nicooo21 <nmagana2112@gmail.com>
2016-08-26 23:07:38 -0700

    Added Comments to Step 4

--- code2040_step4.py
+++ code2040_step4.py
@@ -14,10 +14,10 @@ def main():
 	strings = data["array"]
 	arr_len = len(strings)
 	myList = []
-	for x in range(0, arr_len):
-		match = re.search(r"^%s" % prefix, strings[x])
-		if not match:
-			myList.append(strings[x])
+	for x in range(0, arr_len): 				# iterates through array
+		match = re.search(r"^%s" % prefix, strings[x]) 	# uses regex to find if prefix is in beginning of array
+		if not match:					# if prefix isn't in front of the element
+			myList.append(strings[x])		# appends to array
 	url = 'http://challenge.code2040.org/api/prefix/validate'
 	m_data = { 'token':'4db4fe513b59797d9ed7b6841cfbbf89', 'array':myList }
 	r = requests.post(url, data = json.dumps(m_data), headers=headers)
