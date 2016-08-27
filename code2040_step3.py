475cff48dd6e9a949129fabff34cdd2a1177db9e
nicooo21 <nmagana2112@gmail.com>
2016-08-26 20:54:09 -0700

    Uploaded Step 3
    Step 3 for CODE2040 API Challenge

---
+++ code2040_step3.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import re
+
+def main():
+
+	url = 'http://challenge.code2040.org/api/haystack'
+	m_data = { 'token':'4db4fe513b59797d9ed7b6841cfbbf89' }
+	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+	r = requests.post(url, data=json.dumps(m_data), headers=headers)
+	array = r.content
+	data = json.loads(array)
+	needle = data["needle"]
+	index = 0
+	haystack = data["haystack"]
+	for x in range(0, len(haystack)):
+		if haystack[x] == needle:
+			index = x
+	url = 'http://challenge.code2040.org/api/haystack/validate'
+	m_data = { 'token':'4db4fe513b59797d9ed7b6841cfbbf89' , 'needle':index}		
+	r = requests.post(url, data=json.dumps(m_data), headers=headers)
+	
+
+if __name__ == "__main__":
+	main()
